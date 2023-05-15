da4d45d9be63646bb4770e0b73a5f5bba6db3add
Christina <christina.e.c.barberi@gmail.com>
2023-05-15 19:21:34 -0400

    running deploy

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Header from "../src/components/Header/index";
+import About from "../src/components/About/index";
+import Contact from "../src/components/Contact/index";
+import Portfolio from "../src/components/Portfolio";
+import Resume from "../src/components/Resume";
+import Footer from "../src/components/Footer/index";
+import { Helmet } from "react-helmet";
+import "./App.css";
+
+function App() {
+  const [currentTab, handleTabChange] = useState("about");
+
+  // This method is checking to see what the value of `currentTab` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderTab = () => {
+    if (currentTab === "About") {
+      return <About />;
+    }
+    if (currentTab === "Contact") {
+      return <Contact />;
+    }
+    if (currentTab === "Portfolio") {
+      return <Portfolio />;
+    }
+    if (currentTab === "Resume") {
+      return <Resume />;
+    }
+    return <About />;
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>Christina Barberi's Portfolio | {currentTab} </title>
+      </Helmet>
+      ;
+      <Header
+        currentTab={currentTab}
+        handleTabChange={handleTabChange}
+      ></Header>
+      <main>{renderTab()}</main>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
