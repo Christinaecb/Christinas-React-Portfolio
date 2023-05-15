9d43352feef690091117b7e9b0b01595c57db8f1
Christina <christina.e.c.barberi@gmail.com>
2023-05-15 12:45:56 -0400

    finished about section

---
+++ src/components/About/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import profileImage from "../../assets/Profile-Pic";
+function About() {
+  return (
+    <section id="about-me" className="my-5 intro">
+      <div class="flex-row">
+        <h1 id="about" className="section-title primary-border">
+          About Me
+        </h1>
+      </div>
+      <div className="flex-row">
+        <div class="intro-info">
+          <div class="intro-img">
+            <img src={profileImage} style={{ width: "100%" }} alt="profile" />
+          </div>
+          <p>
+            Placeholder
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
