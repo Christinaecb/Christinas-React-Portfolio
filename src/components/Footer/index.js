74cc22a530f1379d1d94e878f90c97ae70d0bf79
Christina <christina.e.c.barberi@gmail.com>
2023-05-18 12:31:50 -0400

    removed more personal info

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ import { FaInstagram } from "react-icons/fa";
 function Footer() {
   return (
     <footer>
-      <h2>Made by Christina Barberi©</h2>
+      <h2>Made by Christina B©</h2>
       <p>
         <ul>
           <a href="https://github.com/Christinaecb">
